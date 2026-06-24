6f0a012b3d6a4237855a16953b9642a894e7e0e7
Max <66534050+max-silva-12@users.noreply.github.com>
2026-06-24 20:04:10 -0300

    Criar main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
+from config import TELEGRAM_BOT_TOKEN
+from handlers import start, handle_message
+
+def main():
+    app = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+
+    print("Bot rodando...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
